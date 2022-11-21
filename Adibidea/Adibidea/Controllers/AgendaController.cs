a52339c1dcc7547c7b3dc6240380d90725d17621
Raul Parra <parra.raul@uni.eus>
2022-11-21 12:38:40 +0100

    Adibidea

---
+++ Adibidea/Adibidea/Controllers/AgendaController.cs
@@ -0,0 +1,20 @@
+﻿using Adibidea.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adibidea.Controllers
+{
+    public class AgendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            Kontaktua kontaktua = new Kontaktua("13456765", "Raul", "Parra");
+            kontaktua.Emaila = "parra.raul@uni.eus";
+            return View(kontaktua);
+        }
+
+    }
+}
