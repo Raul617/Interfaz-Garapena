3c36b3c682ead2bd8f4700609d11e0e72c9af683
Raul Parra <parra.raul@uni.eus>
2022-11-22 10:08:57 +0100

    Adibide Ariketa Amaituta

--- Adibidea/Adibidea/Controllers/AgendaController.cs
+++ Adibidea/Adibidea/Controllers/AgendaController.cs
@@ -5,9 +5,19 @@ namespace Adibidea.Controllers
 {
     public class AgendaController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
+            List<Kontaktua> lstKontaktuak = new List<Kontaktua>();
+            Kontaktua kontaktua1 = new Kontaktua("13456765", "izaskun", "Kortabitarte");
+            kontaktua1.Emaila = "ikortabitarte@uni.eus";
+            lstKontaktuak.Add(kontaktua1);
+            Kontaktua kontaktua2 = new Kontaktua("13452345", "pepe", "Fernandez");
+            kontaktua2.Emaila = "pfernandez@uni.eus";
+            lstKontaktuak.Add(kontaktua2);
+            Kontaktua kontaktua3 = new Kontaktua("14567874", "jon", "Perez");
+            kontaktua3.Emaila = "jperez@uni.eus";
+            lstKontaktuak.Add(kontaktua3);
+            return View(lstKontaktuak);
         }
         public ActionResult Details()
         {
